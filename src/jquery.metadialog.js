d57de758082487a9a39f4b5e88de0a333338b8af
Eric Hynds <eric@hynds.net>
2010-02-23 11:29:31 -0500

    perf improvements

--- src/jquery.metadialog.js
+++ src/jquery.metadialog.js
@@ -1,10 +1,9 @@
 /*
- * jQuery meta dialog plug-in 1.0
+ * jQuery Meta Dialog Plugin 1.0
  *
  * http://www.erichynds.com
  *
  * Copyright (c) 2009 Eric Hynds
- *
  *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
@@ -13,30 +12,19 @@
  
 (function($){
 	$.fn.metadialog = function(options){
-
-		// default options
-		$.fn.metadialog.defaults = {
-			extraParams: {},
-			loadingHTML: 'Loading...',
-			loadCallback: function(){},
-			reloadParent: false,
-			metadataType: 'class',
-			metadataName: 'metadata'
-		};
 	
 		// merge this plugin's defaults with the dialogs defaults
 		var opts = $.extend({}, $.ui.dialog.defaults, $.fn.metadialog.defaults, options);
 
-		// on the click event of each matched item... live() for nested dialogs
-		return this.live('click', function(e){
+		return this.live("click", function(e){
 			var $this = $(this),
 			
-			// the ID of the dialog window that opened this one, if applicable.
-			parent = $this.parents(".metadialog-dialog").attr('id') || undefined,
-			
+			// the dialog window that opened this one
+			$parent = $this.closest("div.metadialog-dialog"),
+		
 			// override this instance's options with attribute meta data
 			settings = $.metadata ? $.extend({}, opts, $this.metadata({ type:opts.metadataType, name:opts.metadataName }) ) : opts,
-			
+		
 			// create a unique ID for this dialog
 			dialogID = 'metadialog-dialog-' + (new Date).getTime(),
 
@@ -46,31 +34,40 @@
 			.load(this.href, settings.extraParams, settings.loadCallback)
 			.dialog(settings)
 			.dialog("open")
-			.data('postdata', [this.href, settings.extraParams]);
-			
+			.data("postdata", [this.href, settings.extraParams]);
+		
 			// if we are going to attempt to reload the parent dialog, and one actually exists...
-			if(settings.reloadParent && parent !== undefined){
-				
+			if(settings.reloadParent && $parent.length){
+			
 				// bind to this dialog's close event
 				$thisdialog.bind('dialogclose', function(){
-					var $this = $(this),
-					    $parent = $('#' + parent),
-					    postdata = $parent.data('postdata'),
-					    href = postdata[0], 
-					    extraParams = postdata[1];
-					
+					var postdata = $parent.data('postdata'),
+						href = postdata[0], 
+						extraParams = postdata[1];
+				
 					// close the dialog
-					$this.remove();
-
+					$(this).remove();
+					
 					// reload data into the parent if the parent is still open.
-					if($parent.dialog('isOpen')){
+					if($parent.dialog("isOpen")){
 						$parent.load(href, extraParams, settings.loadCallback);
-					}
+					};
 				});
 			};
-			
+		
 			e.preventDefault();
 		});
 	};
+	
+	// default options
+	$.fn.metadialog.defaults = {
+		extraParams: {},
+		loadingHTML: 'Loading...',
+		loadCallback: function(){},
+		reloadParent: false,
+		metadataType: 'class',
+		metadataName: 'metadata'
+	};
+	
 })(jQuery);
 
